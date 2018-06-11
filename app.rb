91be3575bb5d2d388896d158f28ecea164230497
Tech-A27 <tech-a27@Tech-A27s-MacBook-Air.local>
2018-06-11 20:38:38 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+def calculator
+  puts "Which calcualator do you want to use (b)asic or (a)dvanced?"
+  calculator_type = gets.chomp
+  puts "What kind of function do you want? (a)dd (s)ubtract (m)ultiply (d)ivide"
+  function_type = gets.chomp
+
+i = 0
+  if i > 10
+    puts i
+    i+=1
+  end
+
+
+
+
+
+
+
+
+end # function end
